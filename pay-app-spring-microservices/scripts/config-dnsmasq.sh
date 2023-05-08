fa94e03a45b426c74f66abfa220da82d8d5c2b9d
Benjamin Silva <benjamin.silva@insight4.tech>
2023-05-08 08:36:29 -0500

    Service discovery working

--- pay-app-spring-microservices/scripts/config-dnsmasq.sh
+++ pay-app-spring-microservices/scripts/config-dnsmasq.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-echo "\n\nInstalando Dnsmasq------------------------------"
+# Update
+echo "\n\nUpdating packages------------------------------"
+sudo apt-get update
 
 # Instalar dnsmasq
-sudo apt-get update
+echo "\n\nInstalando Dnsmasq------------------------------"
 sudo apt-get install dnsmasq -y
 
 echo "\n\nActualizando /etc/dnsmasq.d/10-consul-------------------------------"
@@ -14,6 +16,10 @@ sudo echo "server=/consul/127.0.0.1#8600" > /etc/dnsmasq.d/10-consul
 # Mostrar los archivos creados
 cat /etc/dnsmasq.d/10-consul
 
+# Detener el servicio por defecto de dns
+sudo systemctl stop systemd-resolved.service
+
+
 # Reiniciar servicio dnsmasq
 sudo systemctl restart dnsmasq
 
@@ -23,12 +29,18 @@ echo "\n\nActualizando /etc/resolv.conf------------------------------"
 # Eliminar el archivo /etc/resolv.conf
 sudo rm -f /etc/resolv.conf
 # Crear el archivo /etc/resolv.conf con la nueva información
-echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf > /dev/null
 echo "nameserver 127.0.0.1" | sudo tee -a /etc/resolv.conf > /dev/null
-echo "nameserver 127.0.0.53" | sudo tee -a /etc/resolv.conf > /dev/null
+echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf > /dev/null
 echo "options edns0 trust-ad" | sudo tee -a /etc/resolv.conf > /dev/null
 echo "search ." | sudo tee -a /etc/resolv.conf > /dev/null
 
 # Reiniciar servicio Network Manager
 sudo systemctl restart NetworkManager
 cat /etc/resolv.conf
+
+# Reiniciar servicio Network Manager
+sudo systemctl restart NetworkManager
+cat /etc/resolv.conf
+
+# Comprobar el estado del servicio
+sudo systemctl status -q dnsmasq.service
--- pay-app-spring-microservices/start.sh
+++ pay-app-spring-microservices/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo ./scripts/clean.sh
-docker-compose -f ./docker/resources.yml up --build -d --remove-orphans
+docker-compose -f ./docker/resources.yml up --build -d
 echo 'Waiting for appconfig up... ... ... ... ...'
 sleep 5
-docker-compose -f ./docker/services.yml up --build -d --remove-orphans
+docker-compose -f ./docker/services.yml up --build -d
 sudo ./scripts/config-dnsmasq.sh
