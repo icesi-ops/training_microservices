1a1a5e6c570ed97a8a288a8fbb8eb903a7f61b59
Benjamin Silva <benjamin.silva@insight4.tech>
2023-05-08 11:44:32 -0500

    Api Gateway config

--- pay-app-spring-microservices/scripts/config-dnsmasq.sh
+++ pay-app-spring-microservices/scripts/config-dnsmasq.sh
@@ -8,23 +8,23 @@ sudo apt-get update
 echo "\n\nInstalando Dnsmasq------------------------------"
 sudo apt-get install dnsmasq -y
 
-echo "\n\nActualizando /etc/dnsmasq.d/10-consul-------------------------------"
 # Eliminar el archivo de configuracion de dnsmasq
+echo "\n\nActualizando /etc/dnsmasq.d/10-consul-------------------------------"
 sudo rm -f /etc/dnsmasq.d/10-consul
+
 # Crear archivo de configuración para consul
 sudo echo "server=/consul/127.0.0.1#8600" > /etc/dnsmasq.d/10-consul
+
 # Mostrar los archivos creados
 cat /etc/dnsmasq.d/10-consul
 
 # Detener el servicio por defecto de dns
 sudo systemctl stop systemd-resolved.service
 
-
 # Reiniciar servicio dnsmasq
 sudo systemctl restart dnsmasq
-
-
 echo "\n\nActualizando /etc/resolv.conf------------------------------"
+
 # Agregar nameserver a /etc/resolv.conf
 # Eliminar el archivo /etc/resolv.conf
 sudo rm -f /etc/resolv.conf
