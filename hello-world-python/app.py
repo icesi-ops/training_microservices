ebeca132e9374722c1a7d1b50d844003ffb10738
icesi-ops <59986692+icesi-ops@users.noreply.github.com>
2023-11-20 16:19:09 -0500

    Update app.py

--- hello-world-python/app.py
+++ hello-world-python/app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def run():
-    return "{\"message\":\"Hello World Python v1\"}"
+    return "<h1>Hello World Python v1</h1>"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
