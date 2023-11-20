6206c9545d8076fcb0978f4aea03b16afd1e783e
icesi-ops <59986692+icesi-ops@users.noreply.github.com>
2023-11-20 16:18:23 -0500

    Update testRoutes.py

--- hello-world-python/testRoutes.py
+++ hello-world-python/testRoutes.py
@@ -2,5 +2,5 @@ from app import *
 
 def test_Home():
     response = app.test_client().get('/')
-    assert b"Welcome" in response.data
+    assert b"Hello World Python v1" in response.data
     assert response.status_code == 200
